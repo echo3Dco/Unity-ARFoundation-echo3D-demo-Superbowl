8f5b58c78c5ba038e8dbc083ecfc5493a32b87ca
Bebe <bebethegirl@gmail.com>
2022-02-13 08:58:11 -0800

    Imported game to new repo. Cleaned up code. Tested game: works.

---
+++ Assets/FieldGoalFolder/Scripts/FieldGoalTrigger.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldGoalTrigger : MonoBehaviour
+{
+
+    [SerializeField]
+    private Rigidbody football;
+
+    [SerializeField]
+    private float footballSpeed;
+
+    [SerializeField]
+    public AudioSource cheeringSource;
+
+    private Touch tapToThrow;
+    private Vector3 startingPosition;
+    private Quaternion startingRotation;
+
+    // Starting point for the football
+    void Start()
+    {
+        startingPosition = football.transform.position;
+        startingRotation = football.transform.rotation;
+    }
+
+    void Update()
+    {
+        // To play on your Android device, comment out this Input. Hit "Space" bar to fire.
+        if (Input.GetKeyDown(KeyCode.Space))
+        
+        // To play on keyboard, comment out this Input. 
+        // if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            football.AddForce(-football.transform.right * footballSpeed, ForceMode.VelocityChange);
+            print("Shooting the shot");
+        }
+    }
+
+    // On field goal trigger collision, play sound and reset ball to starting point
+    void OnTriggerEnter(Collider other)
+    {
+        cheeringSource.Play();
+
+        football.transform.position = startingPosition;
+        football.transform.rotation = startingRotation;
+
+        football.velocity = Vector3.zero;
+    }
+}
---
+++ Assets/echo3D/CustomBehaviour.cs
@@ -0,0 +1,53 @@
+﻿/**************************************************************************
+* Copyright (C) echoAR, Inc. (dba "echo3D") 2018-2021.                    *
+* echoAR, Inc. proprietary and confidential.                              *
+*                                                                         *
+* Use subject to the terms of the Terms of Service available at           *
+* https://www.echo3D.co/terms, or another agreement                       *
+* between echoAR, Inc. and you, your company or other organization.       *
+***************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomBehaviour : MonoBehaviour
+{
+    Rigidbody rb;
+
+    [HideInInspector]
+    public Entry entry;
+
+    /// <summary>
+    /// EXAMPLE BEHAVIOUR
+    /// Queries the database and names the object based on the result.
+    /// </summary>
+
+    void Start()
+    {
+
+        // Add RemoteTransformations script to object and set its entry as long as this object is not named "Football.glb"
+        // if (this.gameObject.name != "football-wilson.glb")
+        // { 
+            this.gameObject.AddComponent<RemoteTransformations>().entry = entry;
+        // } 
+
+        // Query additional data to get the name
+        string value = "";
+        if (entry.getAdditionalData() != null && entry.getAdditionalData().TryGetValue("name", out value))
+        {
+            // Set name
+            this.gameObject.name = value;
+        }
+    }
+
+    void Update()
+    {
+       /* Removed TransformRotations from the imported projectile football
+        
+        * if (this.gameObject.name != "football-wilson.glb" && this.gameObject.GetComponent<RemoteTransformations>().enabled == true)
+        {
+            this.gameObject.AddComponent<RemoteTransformations>().enabled = false;
+            
+        } */ 
+    }
+}
